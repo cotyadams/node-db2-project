c8600dae7f69f49354d1f568d61994ad82ee0d99
cotyadams <cotyadams1@gmail.com>
2024-03-06 23:32:10 -0500

    10 tests passed

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -5,7 +5,7 @@ const { checkCarId, checkCarPayload, checkVinNumberUnique, checkVinNumberValid, 
 router.get('/', (req, res) => {
     getAll()
         .then((cars) => {
-        res.status(200).json(cars)
+        res.status(200).json([...cars])
         }).catch((err) => {
             res.status(500).json({
                 message: `Error getting all cars: ${err}`
